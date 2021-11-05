3c0aed2262883939f0a1fefacce2d20d51aeed60
srissirs <sarareis2002@gmail.com>
2021-11-05 12:11:01 +0000

    Passo 6 (again)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        //  System.out.println("Hello World!");
-       Game g = new Game();
-       g.run();
 
+        //  System.out.println("Hello World!");
+        Game game = new Game();
+        game.run();
     }
 }
 
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,10 @@
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TabBehaviour;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -9,31 +13,38 @@ import java.io.IOException;
 
 
 public class Game {
-    public void Game() throws IOException {
+    private Terminal terminal = new DefaultTerminalFactory().createTerminal();
+    private Screen screen = new TerminalScreen(terminal);
+
+    private int x = 10;
+    private int y = 10;
+
+    public Game() throws IOException {
+
+         /*
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        */
+
         try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
+
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        TerminalSize terminalSize = new TerminalSize(40, 20);
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
-                .setInitialTerminalSize(terminalSize);
-        Terminal terminal = terminalFactory.createTerminal();
-
-        Screen screen = null;
-        screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-        screen.refresh();
     }
 
 
+
     private void draw() throws IOException {
-        Screen screen = null;
+
         screen.clear();
         screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
@@ -42,4 +53,9 @@ public class Game {
         draw();
     }
 
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+    }
+
 }
+
