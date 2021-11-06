2f703d1b288f7a5976af5c080ff4bdadedb3a7cd
srissirs <sarareis2002@gmail.com>
2021-11-06 15:41:58 +0000

    Step 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,6 +3,7 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TabBehaviour;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -13,35 +14,41 @@ import java.io.IOException;
 
 
 public class Game {
-    private Terminal terminal = new DefaultTerminalFactory().createTerminal();
-    private Screen screen = new TerminalScreen(terminal);
+    final private Terminal terminal = new DefaultTerminalFactory().createTerminal();
+    final private Screen screen = new TerminalScreen(terminal);
 
     private int x = 10;
     private int y = 10;
 
-    public Game() throws IOException {
+    private void processKey(KeyStroke key) {
+        if (key.getKeyType()== KeyType.ArrowUp) {
+            y -= 1;
+        } else if (key.getKeyType()== KeyType.ArrowDown) {
+            y += 1;
+        } else if (key.getKeyType()== KeyType.ArrowLeft) {
+            x -= 1;
+        } else if (key.getKeyType()== KeyType.ArrowRight) {
+            x += 1;
+        }
+        System.out.println(key);
+    }
 
+
+
+    public Game() throws IOException {
          /*
         TerminalSize terminalSize = new TerminalSize(40, 20);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
         */
-
         try {
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
-
 
     private void draw() throws IOException {
 
@@ -51,10 +58,16 @@ public class Game {
     }
     public void run() throws IOException {
         draw();
-    }
-
-    private void processKey(KeyStroke key) {
-        System.out.println(key);
+        KeyStroke key= screen.readInput();;
+        while(key.getKeyType()!=KeyType.EOF) {
+            if (key.getKeyType() == KeyType.Character && key.getCharacter()=='q') break;
+            processKey(key);
+            screen.clear();
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+            key = screen.readInput();
+        }
+        screen.close();
     }
 
 }
